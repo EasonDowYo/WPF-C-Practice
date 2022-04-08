595c0dfd638cd72379d99d16f97da8f823102a5a
EasonDowYo <king0906sk@gmail.com>
2022-04-08 21:13:25 +0800

    Add SQL template

---
+++ SQL_Template/SQL_Template/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+namespace SQL_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SQLClass sQLClass = new SQLClass();
+            DataTable dt = new DataTable();
+            string SqlString = "select * from Spend_db.dbo.Spend_Table";
+
+            dt=sQLClass.DoQuery(SqlString);
+
+            Console.WriteLine(dt.Rows[0]["item_name"].ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SQL_Template/SQL_Template/SQLClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace SQL_Template
+{
+    public class SQLClass
+    {
+        private SqlConnection SqlConn;
+        private SqlCommand SqlCmd;
+        private SqlConnectionStringBuilder ConnStrBuilder = new SqlConnectionStringBuilder();
+        public String SqlString = "";
+        public SQLClass()
+        {
+            Connect();
+        }
+        private void Connect()
+        {
+            ConnStrBuilder.DataSource = "LAPTOP-0G73EUQ6\\TEW_SQLEXPRESS";
+            ConnStrBuilder.UserID = "Eason";
+            ConnStrBuilder.Password = "1111";
+            ConnStrBuilder.InitialCatalog = "Spend_db";
+            try
+            {
+                SqlConn = new SqlConnection(ConnStrBuilder.ConnectionString);
+                
+                SqlConn.Open();
+                if (SqlConn.State != ConnectionState.Open)
+                    Console.WriteLine("Connect MSSQL open failed");
+
+
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public DataTable DoQuery(string SqlStr)
+        {
+            DataTable QueryResult = new DataTable();
+            SqlString = SqlStr;
+            SqlCmd = new SqlCommand(SqlStr, SqlConn);
+
+            QueryResult.Load(SqlCmd.ExecuteReader());
+
+
+            return QueryResult;
+        }
+    }
+}
